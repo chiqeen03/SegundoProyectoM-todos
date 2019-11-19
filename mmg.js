c09dc3630b5108695a99991c61dadb9fca18b32b
Manuel Avalos Tovar <manuel.avalos@konfio.mx>
2019-11-19 15:54:59 -0600

    Minor changes

--- mmg.js
+++ mmg.js
@@ -3,5 +3,18 @@ function mmg(llegadas, servicios, servidores){
     let tasaServicios = servicios;
     let s = servidores
 
+    let p0;
     let p;
+    let Lq;
+    let L;
+    let Wq;
+    let W;
+
+    // p
+    p = tasaLlegadas/tasaServicios;
+
+    // P0
+    p0 = 1-p;
+
+    //Lq = ((Math.pow(tasaLlegadas, 2) * ))/(2*(1-p))
 }
